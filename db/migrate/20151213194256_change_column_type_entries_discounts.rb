a4eaaf73373a4192bc4be896fef8ad13d27555de
diana carvalho <dianacarvalho@dianas-MBP.lan>
2015-12-14 19:56:49 +0000

    change migration

--- db/migrate/20151213194256_change_column_type_entries_discounts.rb
+++ db/migrate/20151213194256_change_column_type_entries_discounts.rb
@@ -1,5 +1,5 @@
 class ChangeColumnTypeEntriesDiscounts < ActiveRecord::Migration
   def change
-  	rename_column :entries_discounts, :type, :entry_type
+  	#rename_column :entries_discounts, :type, :entry_type
   end
 end
